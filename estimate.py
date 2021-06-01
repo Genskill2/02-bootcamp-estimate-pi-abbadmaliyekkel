5973fe87d670bf90603718121ae9bf90a2ebe743
Abbad Maliyekkal <84775990+abbadmaliyekkel@users.noreply.github.com>
2021-06-01 16:00:53 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -46,6 +46,5 @@ class TestMC(unittest.TestCase):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
         
-    
 if _name_ == "_main_":
     unittest.main()
